b7dbc0b7af8a6bac2ef1db01096a2b190399d4a7
Wolfgang Moser <6838235+wolfm89@users.noreply.github.com>
2025-11-14 11:20:30 +0100

    refactor(render): extract ray tracing and intensity calculations
    Extract ray tracing logic into `trace_ray()` function and intensity calculation
    into `calc_intensity()` function. Also refactor `image_to_world()` as a separate
    function to improve code modularity and readability. Adjust scene composition
    and rendering parameters for better visual output.

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,14 @@ use crate::{hit::Hittable, sphere::Sphere, vec3::Vec3};
 fn main() {
     let mut scene: Vec<Box<dyn Hittable>> = Vec::new();
 
-    let sphere1 = Sphere::new(Vec3::new(2.0, 0.0, -6.0), 3.0);
-    let sphere2 = Sphere::new(Vec3::new(-3.0, 0.0, -7.0), 3.0);
-    let sphere3 = Sphere::new(Vec3::new(0.0, 2.0, -4.0), 1.0);
+    let sphere1 = Sphere::new(Vec3::new(4.0, 0.0, -5.0), 2.0);
+    let sphere2 = Sphere::new(Vec3::new(-4.0, -2.0, -7.0), 3.0);
+    let sphere3 = Sphere::new(Vec3::new(4.0, 4.0, -9.0), 3.0);
+    let sphere4 = Sphere::new(Vec3::new(0.0, 0.0, -10.0), 4.0);
     scene.push(Box::new(sphere1));
     scene.push(Box::new(sphere2));
     scene.push(Box::new(sphere3));
+    scene.push(Box::new(sphere4));
 
-    render::draw(100, 50, scene);
+    render::draw(100, 45, scene);
 }
--- src/render.rs
+++ src/render.rs
@@ -4,7 +4,7 @@ use crate::{
     vec3::Vec3,
 };
 
-const SHADES: &str = " .'`^\",:;Il!i?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+const SHADES: &str = ".'`^\",:;Il!i?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
 
 const CAMERA_ORIGIN: Vec3 = Vec3 {
     x: 0.0,
@@ -12,7 +12,7 @@ const CAMERA_ORIGIN: Vec3 = Vec3 {
     z: 0.0,
 };
 const LIGHT_DIR: Vec3 = Vec3 {
-    x: 10.0,
+    x: -10.0,
     y: 5.0,
     z: -1.0,
 };
@@ -26,49 +26,21 @@ pub fn draw(width: usize, height: usize, scene: Vec<Box<dyn Hittable>>) {
     let shades: Vec<char> = SHADES.chars().collect();
     let max_idx = shades.len() - 1;
 
-    let aspect = width as f64 / height as f64;
-
     let light_dir = LIGHT_DIR.normalize();
 
     let mut output = String::new();
 
     for y in 0..height {
         for x in 0..width {
-            // map to [-aspect, aspect]
-            let u = ((x as f64 / width as f64) * 2.0 - 1.0) * aspect;
-            // map to range [-1, 1]
-            let v = (y as f64 / height as f64) * 2.0 - 1.0;
-
             // create ray
-            let direction = Vec3::new(u, -v, IMAGE_PLANE_Z);
+            let direction = image_to_world(x, y, IMAGE_PLANE_Z, width, height);
             let ray = Ray::new(CAMERA_ORIGIN, direction);
 
             // calculate hits
-            let mut closest_t = T_MAX;
-            let mut closest_hit: Option<HitRecord> = None;
-
-            for object in scene.iter() {
-                let hit = object.hit(&ray, T_MIN, closest_t);
-                match hit {
-                    Some(h) => {
-                        closest_t = h.t;
-                        closest_hit = Some(h);
-                    }
-                    None => (),
-                }
-            }
+            let hit: Option<HitRecord> = trace_ray(&ray, &scene);
 
             // calculate intensity
-            let intensity: f64;
-            match closest_hit {
-                Some(hit) => {
-                    let diff = hit.normal.dot(&light_dir).max(0.0);
-                    intensity = (AMBIENT + (1.0 - AMBIENT) * diff).clamp(0.0, 1.0);
-                }
-                None => {
-                    intensity = (direction.y + 1.0) * 0.5; // 0..1
-                }
-            }
+            let intensity: f64 = calc_intensity(&light_dir, hit);
 
             // get symbol for intensity
             let idx = ((intensity * (max_idx as f64)) as usize).clamp(0, max_idx);
@@ -76,9 +48,55 @@ pub fn draw(width: usize, height: usize, scene: Vec<Box<dyn Hittable>>) {
 
             // write symbol twice to adjust for characters being around 2x taller than wide
             output.push(symbol);
-            output.push(symbol);
         }
         output.push('\n');
     }
     print!("{}", output)
+}
+
+fn image_to_world(x: usize, y: usize, z: f64, width: usize, height: usize) -> Vec3 {
+    let aspect = width as f64 / height as f64;
+
+    // map to [-aspect, aspect]
+    let u = ((x as f64 / width as f64) * 2.0 - 1.0) * aspect;
+    // map to range [-1, 1]
+    let v = (y as f64 / height as f64) * 2.0 - 1.0;
+
+    Vec3::new(u, -v, z)
+}
+
+fn trace_ray(ray: &Ray, scene: &Vec<Box<dyn Hittable>>) -> Option<HitRecord> {
+    let mut closest_t = T_MAX;
+    let mut closest_hit: Option<HitRecord> = None;
+
+    // calculate hits
+    for object in scene.iter() {
+        let hit = object.hit(&ray, T_MIN, closest_t);
+        match hit {
+            Some(h) => {
+                if h.t < closest_t {
+                    closest_t = h.t;
+                    closest_hit = Some(h);
+                }
+            }
+            None => (),
+        }
+    }
+
+    closest_hit
+}
+
+fn calc_intensity(light_dir: &Vec3, hit: Option<HitRecord>) -> f64 {
+    let intensity: f64;
+    match hit {
+        Some(hit) => {
+            let diff = hit.normal.dot(&light_dir).max(0.0);
+            intensity = (AMBIENT + (1.0 - AMBIENT) * diff).clamp(0.0, 1.0);
+        }
+        None => {
+            // intensity = (direction.y + 1.0) * 0.05; // 0..1
+            intensity = 0.0
+        }
+    }
+    intensity
 }
